b95d64881cf5a8b41250ccc8f5d8e87cdb49dc8b
Jared Sprague <jared@caramelcode.com>
2016-12-10 15:30:06 -0500

    Fixed meshLine

--- src/js/trail.js
+++ src/js/trail.js
@@ -1,9 +1,10 @@
 Game.prototype.Trail = function (game) {
     this.game = game;
-    this.origin = new THREE.Vector3();
+    this.origin = new THREE.Vector3(1);
+
+    // Create the line material
     this.material = new THREE.MeshLineMaterial( {
-        useMap: 0,
-        color: THREE.ColorKeywords.blue,
+        color: new THREE.Color( "rgb(255, 2, 2)" ),
         opacity: 1,
         resolution: new THREE.Vector2( window.innerWidth, window.innerHeight ),
         sizeAttenuation: 1,
@@ -16,21 +17,15 @@ Game.prototype.Trail = function (game) {
         side: THREE.DoubleSide,
         visibility: 1,
     });
-    var geometry = new THREE.Geometry();
-    geometry.vertices.push(this.origin);
-    geometry.vertices.push(this.origin);
-    geometry.vertices.push(this.origin);
-    geometry.vertices.push(this.origin);
-    geometry.vertices.push(this.origin);
-    geometry.vertices.push(this.origin);
-    geometry.vertices.push(this.origin);
-    geometry.vertices.push(this.origin);
-    geometry.vertices.push(this.origin);
-    geometry.vertices.push(this.origin);
-    geometry.vertices.push(this.origin);
-    geometry.vertices.push(this.origin);
-    geometry.vertices.push(this.origin);
+
+    // Create the line geometry used for storing verticies
+    this.trail_geometry = new THREE.Geometry();
+    for (var i = 0; i < 18000; i++) {
+        // must initialize it to the number of positions it will keep or it will throw an error
+        this.trail_geometry.vertices.push(this.origin.clone());
+    }
+
     this.meshLine = new THREE.MeshLine();
-    this.meshLine.setGeometry(geometry, function (p) { return p; });
+    this.meshLine.setGeometry(this.trail_geometry, function (p) { return p; });
     this.mesh = new THREE.Mesh(this.meshLine.geometry, this.material);
 };
